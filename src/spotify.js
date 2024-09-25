f60cf6d893e5837f978dd8b1d61659e3f19acff8
PareshHK <phatikankan@gmail.com>
2024-09-25 11:39:09 +0530

    used .toString method

--- src/spotify.js
+++ src/spotify.js
@@ -4,21 +4,26 @@ const clientId = "4031da59f8204c41b89bffa07f42e3f1";
 
 const scopes = [
     "user-read-currently-playing",
-    "user-read-currently-played",
+    "user-read-recently-played",
     "user-read-playback-state",
-    "user-read-top",
+    "user-top-read",
     "user-modify-playback-state",
 ];
 export const getTokenFromUrl = () => {
     return window.location.hash
     .substring(1)
     .split("&")
-    .reduce((initial,item) =>{
-        let parts = item.split("=")
-        initial[parts[0]] = decodeURIComponent(parts[1])
+    .reduce((initial, item) => {
+        let parts = item.split("=");
+        initial[parts[0]] = decodeURIComponent(parts[1]);
         return initial;
-    },{});
+    }, {});
 };
-export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
-    "%20"
-    )}&response_type=token&show_dialog=true`;
+const params = new URLSearchParams({
+    client_id: clientId,
+    redirect_uri: redirectUri,
+    scope: scopes.join(" "),
+    response_type: "token",
+    show_dialog: "true"
+});
+export const loginUrl = `${authEndpoint}?${params.toString()}`;
